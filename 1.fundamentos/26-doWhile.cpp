54c2f35df4eaa0141e2d8470fbd86586a7f7c9f1
adenilson1 <adenilson99@hotmail.com>
2023-11-21 11:45:46 -0300

    terceira atualização dos programas de fundamentos c++

---
+++ 1.fundamentos/26-doWhile.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int number;
+    
+    do {
+
+        cout << "Enter a number or -1 to finish: ";
+        cin >> number;
+
+    }while(number != -1);
+
+    
+
+    return 0;
+}
+
+/*if(....){
+
+}
+
+while(...){
+
+}
+
+for(;...;){
+
+}
+
+do {
+
+}while(...);*/

bf2f16a57f3a02b42769f9fcd1348b07255b42f4
adenilson1 <adenilson99@hotmail.com>
2023-12-13 14:52:41 -0300

    Função Map

---
+++ 5.array-e-vetores/map.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+using mapfn = int(*)(int);
+
+
+//criando uma funcao map
+vector<int> map(vector<int> v, mapfn fn){  // funcao que transforma cada elemento do vetor
+    vector<int> newVector;
+    for(auto el: v){
+        newVector.push_back((*fn)(el));
+    }
+
+    return newVector;
+
+}
+
+int main() {
+    
+    //funcao quadrada
+    auto square = [](int x) {return x * x;};
+
+    //funcao tripla
+    auto triple= [](int x) {return 3 * x;};
+
+
+    vector<int> nums = {2, 5, 8, 23, 56};
+    //vector<int> squares;
+
+//usando a funcao map
+    vector<int> squares = map(nums, square);
+    vector<int> triples = map(nums, triple);
+
+
+// forma tradicional de mapeamento com laço for
+    /*for(auto num : nums){
+
+        squares.push_back(square(num));
+    }*/
+
+    for(auto n : nums) cout << n << "\t";
+    cout << endl;
+    for(auto sq : squares) cout << sq << "\t";
+    cout << endl;
+    for(auto t : triples) cout << t << "\t";
+
+    return 0;
+}
